c7cd90b7140c8591b06441495bda95fb4c4ad5f8
Marcus-Peterson <73422974+Marcus-Peterson@users.noreply.github.com>
2021-12-05 16:13:57 +0100

    Minor changes to readme.md

--- datamanager.py
+++ datamanager.py
@@ -1,9 +1,11 @@
 import csv
 import datetime
+
+
 class Data:
-    #Display undordered data from a csv file
+    # Display undordered data from a csv file
     @classmethod
-    def data_display(cls,filename):
+    def data_display(cls, filename):
         while True:
             try:
                 with open(filename) as file:
@@ -15,25 +17,25 @@ class Data:
                 print("File couldn't be located, try again")
             except FileExistsError:
                 print("File doesn't exist, try again or create a new file")
-    
-     
+
+
 class Trades_Data():
-    
-    @staticmethod                       #This method is not supposed to be used outside of class
-    def close_data_file(cls,file):
+
+    @staticmethod  # This method is not supposed to be used outside of class
+    def close_data_file(cls, file):
         try:
             with filename as csv_file:
                 csv_file.close()
         except Exception:
             print("Error occured when trying to close file")
-            
 
     @classmethod
-    def amt_winnings(cls,filename):         #Enabling user to display winnings according to time periods
+    # Enabling user to display winnings according to time periods
+    def amt_winnings(cls, filename):
         try:
             with open(filename, "r") as csv_file:
                 csv_reader = csv.reader(csv_file)
-            
+
                 for line in csv_reader:
                     if line[1] < line[3]:
                         continue
@@ -51,9 +53,9 @@ class Trades_Data():
             print("Wrong datatype")
 
     @classmethod
-    def amt_losses(cls,filename):
+    def amt_losses(cls, filename):
         try:
-            with open(filename,"r") as csv_file:
+            with open(filename, "r") as csv_file:
                 csv_reader = csv.reader(csv_file)
 
                 for line in csv_reader:
@@ -61,33 +63,33 @@ class Trades_Data():
                         continue
                     else:
                         print("Your losses")
-                        print(line) 
+                        print(line)
         except FileExistsError:
             print("File doesn't exists")
         except FileNotFoundError:
             print("Could not locate file")
         except ValueError:
             print("Wrong datatype")
-        
 
     @classmethod
-    def winnings_periods(cls,filename):
-        
+    def winnings_periods(cls, filename):
+
         try:
             with open(filename, "r") as csv_file:
                 csv_reader = csv.reader(csv_file)
-                user_request = (input("Request period: days, weeks, months, years"))
+                user_request = (
+                    input("Request period: days, weeks, months, years"))
                 if user_request == "days":
                     user_days = int(input("How many days? \n>>>"))
                     amt_days = datetime.timedelta(days=user_days)
-                    pass #As of now, csv_reader and amt_days are unused variables
+                    pass  # As of now, csv_reader and amt_days are unused variables
         except FileExistsError:
             print("File doesn't exists")
         except FileNotFoundError:
             print("Could not locate file")
         except ValueError:
             print("Wrong datatype")
-                    
-                    
+
 
 Trades_Data.amt_winnings("trades.csv")
+# testa
